a7303aac8779954c1f73eee386886b904363dec3
@nataliia.vyrsta <natali.vyrsta@gmail.com>
2023-01-20 22:10:47 +0200

    add sum of two positive integers

---
+++ JavaScript/7kyu/sum_of_two_lowest_positive_integers/sum_of_two_lowest_positive_integers.js
@@ -0,0 +1,6 @@
+function sumTwoSmallestNumbers(numbers) {
+  return numbers.sort((a, b) => a - b)[0] + numbers[1];
+}
+
+console.log(sumTwoSmallestNumbers([19, 5, 42, 2, 77])); // 7
+console.log(sumTwoSmallestNumbers([10, 343445353, 3453445, 3453545353453])); // 3453455
