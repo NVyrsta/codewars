992faeefd85fd22b9518325791925b3f5c864033
@nataliia.vyrsta <natali.vyrsta@gmail.com>
2023-02-06 14:28:37 +0200

    add roman numerals decoder

---
+++ JavaScript/6kyu/roman_numerals_decoder/roman_numerals_decoder.js
@@ -0,0 +1,21 @@
+function solution(str) {
+  let num = 0;
+  const roman = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  };
+
+  for (let i = 0; i < str.length; i++) {
+    const curr = roman[str[i]];
+    const next = roman[str[i + 1]];
+
+    curr < next ? (num -= curr) : (num += curr);
+  }
+
+  return num;
+}
